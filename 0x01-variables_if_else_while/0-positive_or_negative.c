be3376c5b285ba02b4dc5e61b58e1edca5719577
belkerf <jbelkerf@gmail.com>
2023-10-14 09:57:44 -0700

    update

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	else if (n == 0)
+	{
+		printf("is zero\n");
+	}
+	else
+	{
+		printf("is negative\n");
+	}
+}
