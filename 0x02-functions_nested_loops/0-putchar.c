0b85e0d40c5a5e8dc20a046d7ea659619d17ea14
belkerf <jbelkerf@gmail.com>
2023-10-14 15:44:35 -0700

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - the entry point of our program
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
