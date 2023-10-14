787b437278aae4c399df3a1b39d748a0f5b255a3
belkerf <jbelkerf@gmail.com>
2023-10-14 11:33:50 -0700

    update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - the entry point of our program
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	int i = 61;
+
+	/*printing lowercase charachers*/
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+		if (i == 123)
+		{
+			i = 65;
+			/*printing upercase charachters*/
+			while (i <= 90)
+			{
+				putchar(i);
+				i++;
+				if (i == 91)
+				{
+					putchar('\n');
+					i = 200;
+				}
+			}
+		}
+	}
+	return (0);
+}
