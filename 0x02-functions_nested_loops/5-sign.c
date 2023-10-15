2a3e3169172fcb1bb0f92c9e576880792702d1ab
belkerf <jbelkerf@gmail.com>
2023-10-15 09:29:20 -0700

    adding a  function that prints the sign of a number.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sing - function that prints the sign of a number.
+ * Return: 1 if greater than 0 ,0 if 0, -1 if less than 0.
+ * @n : the number we want to check
+ */
+int print_sing(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
