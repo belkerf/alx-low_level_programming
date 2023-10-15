9df1ca7f5e340c2cc72d42cfaec3ed66dddbd199
belkerf <jbelkerf@gmail.com>
2023-10-15 09:32:11 -0700

    fixing a dump error

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_sing - function that prints the sign of a number.
+ * print_sign - function that prints the sign of a number.
  * Return: 1 if greater than 0 ,0 if 0, -1 if less than 0.
  * @n : the number we want to check
  */
-int print_sing(int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
