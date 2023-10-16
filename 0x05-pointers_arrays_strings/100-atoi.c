64dbab60a52c6c6356cb49d1a75dba8cbaefd7b4
belkerf <jbelkerf@gmail.com>
2023-10-16 16:01:57 -0700

    Writing a function that prints half of a string, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _atoi - a function that convert a string to an integer.
+ * Return : the integernwe convert.
+ * @s : the string we want convert.
+ */
+int _atoi(char *s)
+{
+	/*the number in ascii 0 => 48 && 9=> 57*/
+	int i = 0;
+
+	while (s[i])
+	{
+		if ((s[i]) == '0')
+		{
+
+		}
+		i++;
+	}
+}
