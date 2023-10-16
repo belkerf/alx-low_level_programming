2dcb269b2958216ef4290226201d098afec0cb86
belkerf <jbelkerf@gmail.com>
2023-10-16 09:09:01 -0700

    fixing an error

---
+++ 0x05-pointers_arrays_strings/_strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string.
+ * Return: the length of a string.
+ * @s : the string.
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+	{
+		a++;
+	}
+	return (a);
+}
+
