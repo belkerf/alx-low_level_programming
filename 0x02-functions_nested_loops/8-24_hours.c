fd4aba5785c6b363a17e5d315d39a4e14e41c2e9
belkerf <jbelkerf@gmail.com>
2023-10-17 03:02:13 -0700

    debuging

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,17 +7,18 @@ void jack_bauer(void)
 {
 	int a = 0, b = 0, c = 0, d = 0;
 
-	for (a = 0;a <= 2; a++)
+	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
 			if ((a == 2) && (b == 4))
 			{
-				b = 10;
+				b = 100;
+				a = 100;
 			}
 			for (c = 0; c < 6; c++)
 			{
-				for (d = 0;d <= 9; d++)
+				for (d = 0; d <= 9; d++)
 				{
 					_putchar('0' + a);
 					_putchar('0' + b);
