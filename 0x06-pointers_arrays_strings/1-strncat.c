8a0fafdcbca45f378b6f3ffd2b8a61a8d94722ac
belkerf <jbelkerf@gmail.com>
2023-10-18 11:00:12 -0700

    creating a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,4 +2,26 @@
 /**
  * _strncat - function that concatenates two strings.
  * Return: the result string.
- *
+ * @dest : the first string.
+ * @src : the second string.
+ * @n : the byts we should return from src.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	/*finding the end of dest*/
+	while (dest[i])
+	{
+		i++;
+	}
+	/*adding n byte from src to dest*/
+	while (j < n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	/*returning the result string that is stored in dest*/
+	return (dest);
+}
