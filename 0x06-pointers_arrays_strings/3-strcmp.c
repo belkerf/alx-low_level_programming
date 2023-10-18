ae48952487077617c7294d1b96a24e484c8e4ace
belkerf <jbelkerf@gmail.com>
2023-10-18 15:14:16 -0700

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,27 +7,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0;
-
-	/*first of all we check if s1 and are found*/
-	if ((s1[0] != '\0') && (s2[0] != '\0'))
+	while ((*s1 != '\0') && (*s2 != '\0'))
 	{
-		/*A) counting the size of s1 and s2*/
-		while (s1[i])
-			i++;
-		while (s2[j])
-			j++;
-		/*B) comparing the sizes*/
-			/*B-I) if s1 biger than s2 return 15*/
-		if (i > j)
-			return (15);
-			/*B-II) if s1 less than s2 return -15*/
-		else if (i < j)
-			return (-15);
-			/*B-III) if s1 equal s2 return 0*/
-		else
-			return (0);
+		s1++;
+		s2++;
 	}
-	else
-		return (-1);
+	return (s1 - s2);
 }
