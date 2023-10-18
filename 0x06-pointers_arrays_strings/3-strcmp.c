47e6cfe8e8231cdfe3588a90bccdec46dc170b3a
belkerf <jbelkerf@gmail.com>
2023-10-18 13:35:23 -0700

    updating the code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,19 +9,23 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0, j = 0;
 
-	/*A) counting the size of s1 and s2*/
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
-	/*B) comparing the sizes*/
-		/*B-I) if s1 biger than s2 return 15*/
-	if (i > j)
-		return (15);
-		/*B-II) if s1 less than s2 return -15*/
-	else if (i < j)
-		return (-15);
-		/*B-III) if s1 equal s2 return 0*/
-	else
-		return (0);
+	/*first of all we check if s1 is found*/
+	if (s1[0])
+	{
+		/*A) counting the size of s1 and s2*/
+		while (s1[i])
+			i++;
+		while (s2[j])
+			j++;
+		/*B) comparing the sizes*/
+			/*B-I) if s1 biger than s2 return 15*/
+		if (i > j)
+			return (15);
+			/*B-II) if s1 less than s2 return -15*/
+		else if (i < j)
+			return (-15);
+			/*B-III) if s1 equal s2 return 0*/
+		else
+			return (0);
+	}
 }
