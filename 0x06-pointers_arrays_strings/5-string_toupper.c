eea679220f33fe1510eb37c0fa7b57709d62d67c
belkerf <jbelkerf@gmail.com>
2023-10-19 12:11:43 -0700

    update

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,15 +7,13 @@
  */
 char *string_toupper(char *c)
 {
-	int i = 0;
-
-	while (c[i])
+	while (*c)
 	{
-		if ((c[i] >= 97) && (c[i] <= 122))
+		if ((*c >= 'a') && (*c <= 'z'))
 		{
-			c[i] = (c[i] - 32);
+			*c = (*c - 32);
 		}
-		i++;
+		c++;
 	}
 	return (c);
 }
