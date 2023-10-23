2bfcf799b255e2634085e41a09fdc8985e61af90
belkerf <jbelkerf@gmail.com>
2023-10-23 10:02:45 -0700

    update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,14 +8,26 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 1;
+	int i, j, f;
 
-	while (*s)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		if (*s == *accept)
-			return (i);
-		s++;
+		j = 0;
+		f = 1; /*flag status*/
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				f = 0; /*success*/
+				break;
+			}
+			j++;
+		}
+		if (f == 1)
+			break;
 		i++;
 	}
+
 	return (i);
 }
