733ccaa4d124d5c025b6aa9366c199ea61e7a09c
belkerf <jbelkerf@gmail.com>
2023-10-25 04:12:01 -0700

    creating the directory and the main.h and the first function in the project

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string, followed by a new line.
+ * @s : the string we workin on.
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+	_puts_recursion(s++);
+}
