8c79f2795020d9a88b583b9e31a7e7bb9add1208
belkerf <jbelkerf@gmail.com>
2023-10-26 10:37:17 -0700

    update

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,8 +8,10 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	i += _strlen_recursion(s + 1);
-	if (*s)
+	if (*s == '\0')
+		i = 1;
+	else if (*s && *s != '\0')
+		_strlen_recursion(s + 1);
 		i++;
 	return (i);
 }
