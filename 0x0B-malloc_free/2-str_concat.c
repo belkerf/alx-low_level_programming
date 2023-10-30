12099c5bf47ff329f6dac753a09bf288691d940b
belkerf <jbelkerf@gmail.com>
2023-10-30 14:40:17 -0700

    fixing

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,36 +13,13 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, len, i = 0, j;
 	char *p;
 
-	if (s1[0] != '\0' || s1 != NULL)
-	{
-		while ( s1[len1])
-			len1++;
-		len1++;
-
-	}
-	if (*s2 != '\0' || s2 != NULL)
-	{
-		while (s2[len2])
-			len2++;
-		len2++;
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (len1 = 1; s1[len1 - 1]; len1++)
+	for (len2 = 1; s2[len2 - 1]; len2++)
 	len = len1 + len2 + 1;
-	if (*s2 == '\0' || s2 == NULL)
-	{
-		p = (char *)malloc(len1 * sizeof(char));
-		for (i = 0; i < len1 - 1; i++)
-			*(p + i) = s1[i];
-		*(p + i) = '\0';
-		return (p);
-	}
-	else if (s1[0] == '\0' || s1 == NULL)
-	{
-		p = (char *)malloc(len2 * sizeof(char));
-		for (i = 0; i < len2 - 1; i++)
-			*(p + i) = s2[i];
-		*(p + i) = '\0';
-		return (p);
-	}
 	p = (char *)malloc(len * sizeof(char));
 	if (p == NULL)
 		return (0);
