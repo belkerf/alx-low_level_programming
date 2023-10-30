f61add82db512d1679fcc3cd4ab8d3770d43975c
belkerf <jbelkerf@gmail.com>
2023-10-30 12:29:20 -0700

    fixing

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	arr = (char *)malloc(size * sizeof(char));
+	if (size == 0)
+		return ('\0');
 	while (i < size)
 	{
 		arr[i] = c;
