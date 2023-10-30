ce12c7320637b8aa31b8eaed0c16cef280bf3241
belkerf <jbelkerf@gmail.com>
2023-10-30 14:54:42 -0700

    fixing

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,16 +13,6 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, len, i = 0, j = 0;
 	char *p;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-		len1 = 0;
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-		len2 = 0;
-	}
 	for (len1 = 1; s1[len1 - 1]; len1++)
 	for (len2 = 1; s2[len2 - 1]; len2++)
 	len = len1 + len2 + 1;
