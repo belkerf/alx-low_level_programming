0a05c43cbbf76e4e2f8950da2400322f65d2c2a8
belkerf <jbelkerf@gmail.com>
2023-11-05 14:51:02 +0100

    debugging

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,21 +9,27 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, len1, len;
+	unsigned int i, len1, len2, len;
 	char *arr;
 
-	len1 = len(s1);
-	len = len1 + n + 1;
+	for (len1 = 0; s1[len1]; len1++)
+	len1++;
+	for (len2 = 0; s2[len2]; len2++)
+	len2++;
+	if (n >= len2)
+		len = len1 + len2;
+	else
+		len = len1 + n;
 	arr = (char *)malloc(len * sizeof(char));
 	if (arr == NULL)
-		return"('NULL');
+		return (NULL);
 	for (i = 0; i < len; i++)
 	{
 		if (s1[i] == '\0')
 			continue;
 		else if (i < len1)
 		{
-			*arr = s1(i);
+			*arr = s1[i];
 		}
 		else
 			*arr = s2[i];
