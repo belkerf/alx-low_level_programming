9c97721f3eb329f9e1a678bb0f7ddfc067db57a1
belkerf <jbelkerf@gmail.com>
2023-11-07 22:22:00 +0000

    update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,10 +10,10 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if ((*d).name == NULL)
-		(*d).name = "nil";
+		(*d).name = "(nil)";
 	printf("Name: %s\n", (*d).name);
 	printf("Age: %f\n", (*d).age);
 	if ((*d).owner == NULL)
-		(*d).owner = "nil";
+		(*d).owner = "(nil)";
 	printf("Owner: %s\n", (*d).owner);
 }
