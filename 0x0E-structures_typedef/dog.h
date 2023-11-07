1c3722ae274ec527fba128897f697b32805a197b
belkerf <jbelkerf@gmail.com>
2023-11-07 13:10:39 -0800

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog {
+typedef struct dog {
         char *name;
         float age;
         char *owner;
 };
-#endif                                                                  ~                                                                           ~          
+#endif
