44c7125f96ab72306591ab08ebf7be399dde06ed
belkerf <jbelkerf@gmail.com>
2023-11-08 16:54:29 +0000

    update

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+#include <stddef.h>
 /**
  * array_interator - function that executes a function given as a
  * parameter on each element of an array.
