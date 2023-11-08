21028649efd80682419ef8c8c817fd992e6d390c
belkerf <jbelkerf@gmail.com>
2023-11-08 17:48:10 +0000

    update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,11 +10,14 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	
-	if (!array || !cmp)
-		return;
-	for (i = 0; i < size; i++)
+
+	if (array && cmp)
 	{
-		cmp(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
 	}
+	return (-1);
 }
